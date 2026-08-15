a39a58bff9230ebf2a389045346292a7f5e511ed
authenticcheesekurt-prog <authentic.cheesekurt@gmail.com>
2026-08-15 02:25:09 +0000

    Added the deposit features

--- ATM_Activity.py
+++ ATM_Activity.py
@@ -1,7 +1,4 @@
-def withdraw(self):
-        amount = float(input("Enter amount to be Withdrawn: "))
-        if self.balance >= amount:
-            self.balance -= amount
-            print("\nYou Withdrew:", amount)
-        else:
-            print("\nInsufficient balance")
+def deposit(self):
+        amount = float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\nAmount Deposited:", amount)
