c518de064542546775d8daadf66718dbf2ce3aad
authenticcheesekurt-prog <authentic.cheesekurt@gmail.com>
2026-08-15 02:33:45 +0000

    Added withdraww

--- ATM_Activity.py
+++ ATM_Activity.py
@@ -1,4 +1,7 @@
-def deposit(self):
-        amount = float(input("Enter amount to be Deposited: "))
-        self.balance += amount
-        print("\nAmount Deposited:", amount)
+def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance >= amount:
+            self.balance -= amount
+            print("\nYou Withdrew:", amount)
+        else:
+            print("\nInsufficient balance")
