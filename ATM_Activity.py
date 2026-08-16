66733c57eeddc4aeaf44565d8357903bf133aac6
Persephone <kurtuson92@gmail.com>
2026-08-16 05:16:46 +0000

    Combined ATM features

--- ATM_Activity.py
+++ ATM_Activity.py
@@ -1,53 +1,60 @@
-# PIN Feature
+#pin value
 CORRECT_PIN = "1234"
 
-# User input
+#acc balance
+balance = 10000
+
+
+#input for pin
 user_input = input("Enter your 4-digit PIN: ")
 
-# Checks if matches the PIN
+#for matching pin and input pin
 if user_input == CORRECT_PIN:
     print("Access Granted!")
+
+    # Main Menu
+    while True:
+        print("========== ATM MACHINE ==========")
+        print("1. Balance Check")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+
+        choice = int(input("Select Option (1-4): "))
+
+        # Balance Check
+        if choice == 1:
+            print("Current Balance:", balance)
+
+        # Deposit
+        elif choice == 2:
+            amount = float(input("Enter amount to be Deposited: "))
+
+            if amount > 0:
+                balance += amount
+                print("Amount Deposited:", amount)
+                print("New Balance:", balance)
+            else:
+                print("Invalid amount")
+
+        # Withdraw
+        elif choice == 3:
+            amount = float(input("Enter amount to be Withdrawn: "))
+
+            if balance >= amount:
+                balance -= amount
+                print("You Withdrew:", amount)
+                print("Remaining Balance:", balance)
+            else:
+                print("Insufficient balance")
+
+        # Exit
+        elif choice == 4:
+            print("Goodbye")
+            break
+
+        else:
+            print("Invalid")
+
 else:
     print("Access Denied. Incorrect PIN.")
-
-
-# Withdraw
-def withdraw(self):
-    amount = float(input("Enter amount to be Withdrawn: "))
-    if self.balance >= amount:
-        self.balance -= amount
-        print("\nYou Withdrew:", amount)
-    else:
-        print("\nInsufficient balance")
-
-
-# Deposit
-def deposit(self):
-    amount = float(input("Enter amount to be Deposited: "))
-    self.balance += amount
-    print("\nAmount Deposited:", amount)
-
-
-# Main Menu
-def main_menu():
-    print("========== ATM MACHINE ==========")
-    print("1. Balance Check")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
-
-    choice = int(input("Select Option (1-4): "))
-
-    if choice == 1:
-        print("You selected Balance Check")
-    elif choice == 2:
-        print("You selected Deposit")
-    elif choice == 3:
-        print("You selected Withdraw")
-    elif choice == 4:
-        print("Goodbye")
-    else:
-        print("Invalid")
-
-
-main_menu()
